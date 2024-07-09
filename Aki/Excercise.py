c2313df213ad638abd57ea02f5e8d8ba69bb43ca
Autumn <104688256+Denz001@users.noreply.github.com>
2024-07-09 19:50:11 +0800

    Cleaner code

--- Aki/Excercise.py
+++ Aki/Excercise.py
@@ -1,13 +1,15 @@
-print('\nHi! Welcome to my Python excercise notebook!\n')
+print('\nHi! Welcome to my Python exercise notebook!\n')
 list = ('''
 \n\tPython Excercise List\n
-      1. The F String\n''')
-user_input = int(input(f'Here\'s a list of my Python excercises. {list} \nWhich Python excercise do you want to see?  '))
+    1. The F String\n''')
+
+user_input = int(input(f'''Here\'s a list of my exercises. {list} 
+\n Which do you want to see? '''))
 
 def f_str():
     print('\n\tThis is the excercise for \'The F String\'\n')
     name = input('What\'s your name?\n')
-    year = input('What\'s the year today?\n')
+    year = int(input('What\'s the year today?\n'))
     print(f'Hello {name} from the year {year}!\n')
 
 if user_input == 1: f_str()

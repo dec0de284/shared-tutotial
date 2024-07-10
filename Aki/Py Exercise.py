97b9dcd811affe8ac7ea94832d61260d5c1c7e66
Autumn <104688256+Denz001@users.noreply.github.com>
2024-07-11 02:32:45 +0800

    added

--- Aki/Py Exercise.py
+++ Aki/Py Exercise.py
@@ -2,7 +2,8 @@ print('\n\t Hi! Welcome to my Python exercise notebook!\n')
 
 list = ('''
 \n\tPython Excercise List\n
-    1. The F String\n''')
+    1. The F String\n
+    2. String Index (PW recommender)\n''')
 
 user_input = int(input(f'''Here\'s a list of my exercises. {list} 
 \n Which do you want to see? '''))
@@ -14,5 +15,18 @@ def f_str():
     year = int(input('What\'s the year today?\n'))
     print(f'Hello {name} from the year {year}!\n')
 
+
+def indx():
+    print('\n\tThis is the exercise for \'String Index\'\n')
+    user_input1 = input('What\'s your username? \n')
+    password = input('What\'s your password? \n')
+    length = str(len(user_input1) + len(password))
+    reco = ((user_input1 + length)[::-1])
+    print(f'Recommended password: {reco}{length}')
+    
+
+
 if user_input == 1: 
     f_str()
+if user_input == 2:
+    indx()

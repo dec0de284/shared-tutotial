584c8ef7d71d93f5fa1e7b824999a20b2f126226
Autumn <104688256+Denz001@users.noreply.github.com>
2024-07-15 04:17:51 +0800

    new one

--- Aki/Py Exercise.py
+++ Aki/Py Exercise.py
@@ -4,10 +4,8 @@ list = ('''
 \n\tPython Excercise List\n
     1. The F String\n
     2. String Index (PW recommender)\n
-    3. Immutable\n''')
-
-user_input = int(input(f'''Here\'s a list of my exercises. {list} 
-\n Which do you want to see? '''))
+    3. String Methods\n
+    4. String Methods Pt. 2\n''')
 
 
 def f_str():
@@ -16,6 +14,7 @@ def f_str():
     year = int(input('What year were you born?\n'))
     age = 2024 - year
     print(f'Hello {name}! born in {year}. You are {age} now!\n')
+    return
 
 
 def indx():
@@ -25,9 +24,66 @@ def indx():
     length = str(len(user_input1) + len(password))
     reco = ((user_input1 + length)[::-1])
     print(f'Recommended password: {reco}{length}')
+    return
+
+
+def strmeds():
+    name = 'Aki'
+    user_input1 = input(f'\n{name} is the current username. Do you want to replace it? (Y/N) \n')
+    if user_input1 == 'Y' or user_input1 == 'y':
+        new_name  = input('What is your name? \n')
+        new_name1 = name.replace('Aki', new_name)
+        print(f'\nWelcome, {new_name1}! A great adventure is just up ahead!\n')
+    if user_input1 == 'N' or user_input1 == 'n':
+        print(f'\nOkay, your username remains as {name}. Best wishes on your travels!\n ')
+        return
+
+
+def strmeds1():
+    print('\n\tCorrect the lyrics')
+
+    lyrics = ('''
+    When I was a young boy
+    My father took me into the city
+    To see a marching band
+    He said, "Son, when you grow up
+    Would you be the savior of the normies
+    The uglies and the damned?" \n''')
+
+    print(lyrics)
+
+    wrong_lyric = str(input('What\'s the wrong word? \n'))
+
+    if wrong_lyric in lyrics and len(wrong_lyric) >= 6:
+         right_lyric = str(input('What do you want to replace it with? \n'))
+         new_lyrics = lyrics.replace(wrong_lyric,right_lyric)
+    else:
+        print('\nThat word is not in the lyrics\n')
+        return
     
+    another_one = input('Is there another wrong word? (Y/N)\n')
+    if another_one == 'Y' or another_one == 'y':
+        wrong_lyric1 = input('What\'s the other word? \n')
+        if len(wrong_lyric1) >= 6:
+            right_lyric1 = input('What do you want to replace it with? \n')
+            print(new_lyrics.replace(wrong_lyric1,right_lyric1))
+        else:
+            print('\nThat word is not in the lyrics.\n')
+            return
+    elif another_one == 'N' or another_one == 'n':
+        print(new_lyrics)
+        return
+
+user_input = int(input(f'''Here\'s a list of my exercises. {list} 
+\n Which exercise do you want to see? (Select a number) \n'''))
 
 if user_input == 1: 
     f_str()
 if user_input == 2:
     indx()
+if user_input == 3:
+    strmeds()
+if user_input == 4:
+    strmeds1()
+else:
+    print('That is not in the list')
